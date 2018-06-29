9515ccde1723ae056ddd33fd79ee939c6c7161a6
so6370 <so6370@SO-21M0736.coa.gov.tw>
2018-06-29 18:29:59 +0800

    Add load_insurance method part 1

--- workForceServey/initOfficialData.py
+++ workForceServey/initOfficialData.py
@@ -3,6 +3,7 @@ Created on 2018年6月29日
 
 @author: so6370
 '''
+import xlrd
 
 monthlyEmployee_dict = {}
 
@@ -12,8 +13,38 @@ def load_monthly_employee():
         farm_id = sample_lsit[0].strip()
         monthlyEmployee_dict[farm_id] = sample_lsit
 
+def load_insurance():
+    wb = xlrd.open_workbook('..\\..\\input\\insurance.xlsx')
+    sheet = wb.sheet_by_index(0)
+    distinct_dict = {}
+    
+    for i in range(1, sheet.nrows):
+        row = sheet.row_values(i)
+        id = row[0]
+        id_type = id + '-' + row[1]
+        
+        if not id_type in distinct_dict:
+            value = int(row[2])
+            type = int(row[1])
+            
+            if type == 60 or type == 66:
+                pass
+            
+            else:
+                distinct_dict[id_type] = value * 12
+    
+#     for e in distinct_dict:
+#         print(e, ':', distinct_dict[e])
+    
+    
+    annuity = [45, 48, 35, 36, 37, 38, 55, 56, 57, 59]
+    sheet = wb.sheet_by_index(1)
+    count = 0
+    prev_id = ''
+    prev_value = 0
 
-load_monthly_employee()
+load_insurance()
 
-# for e in monthlyEmployee_dict:
-#     print(e, ':', monthlyEmployee_dict[e])
+
+
+    
