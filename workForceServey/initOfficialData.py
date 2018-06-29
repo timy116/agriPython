c6d9b27287cf1b75b02787a08be88e6a3307a066
so6370 <so6370@SO-21M0736.coa.gov.tw>
2018-06-29 11:41:05 +0800

    Add load_monthly_employee method

--- workForceServey/initOfficialData.py
+++ workForceServey/initOfficialData.py
@@ -3,3 +3,17 @@ Created on 2018年6月29日
 
 @author: so6370
 '''
+
+monthlyEmployee_dict = {}
+
+def load_monthly_employee():
+    for line in open('..\\..\\input\\106_MonthlyEmployee.txt', 'r', encoding = 'utf-8', errors = 'ignore'):
+        sample_lsit = line.strip().split('\t')
+        farm_id = sample_lsit[0].strip()
+        monthlyEmployee_dict[farm_id] = sample_lsit
+
+
+load_monthly_employee()
+
+# for e in monthlyEmployee_dict:
+#     print(e, ':', monthlyEmployee_dict[e])
