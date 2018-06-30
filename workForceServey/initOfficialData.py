bcb5ea91eba4729b7d973d2c706130e58de0a040
Timy <timy117@gmail.com>
2018-06-30 12:28:53 +0800

    Modified load_insurance method

--- workForceServey/initOfficialData.py
+++ workForceServey/initOfficialData.py
@@ -20,14 +20,14 @@ def load_insurance():
     
     for i in range(1, sheet.nrows):
         row = sheet.row_values(i)
-        id = row[0]
-        id_type = id + '-' + row[1]
+        farm_id = row[0]
+        id_type = farm_id + '-' + row[1]
         
         if not id_type in distinct_dict:
             value = int(row[2])
-            type = int(row[1])
+            insurance_type = int(row[1])
             
-            if type == 60 or type == 66:
+            if insurance_type == 60 or insurance_type == 66:
                 pass
             
             else:
@@ -35,16 +35,49 @@ def load_insurance():
     
 #     for e in distinct_dict:
 #         print(e, ':', distinct_dict[e])
-    
+    del distinct_dict
     
     annuity = [45, 48, 35, 36, 37, 38, 55, 56, 57, 59]
     sheet = wb.sheet_by_index(1)
     count = 0
     prev_id = ''
     prev_value = 0
-
+    
+    for i in range(1, sheet.nrows):
+        row = sheet.row_values(i)
+        farm_id = row[0]
+        insurance_type = int(row[1])
+        value = int(row[2])
+        
+        if prev_id == '':
+            prev_id = farm_id
+    
+        if insurance_type in annuity:
+            pay = value
+            count += 1
+            
+            if not id == prev_id:
+                prev_id = farm_id
+                prev_value = value
+                pay = prev_value * (13 - count)
+                count = 0
+            
+            else:
+                pass
+    
+    sheet = wb.sheet_by_index(2)
+    for i in range(1, sheet.nrows):
+        row = sheet.row_values(i)
+        farm_id = row[0]
+        value = row[2]
+        
+    sheet = wb.sheet_by_index(3)
+    for i in range(1, sheet.nrows):
+        row = sheet.row_values(i)
+        farm_id = row[0]
+        value = row[2]
+        
 load_insurance()
-
 
 
     
